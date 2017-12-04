c2d951bbd582bf6e24bde133daac2cde66b92fec
Mhirra Yung <lookatdmoon@gmail.com>
2017-12-03 22:57:09 -0600

    second commit

---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Au Cheval Burger', TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('The Famous Kuma', FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Black Sabbath', FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Lair of the Minotaur', FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('DMK Burger', TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Billy Goat Cheezborger', TRUE);
+
