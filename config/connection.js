bdefbc59e976afb8fe553abc3db884945087fd00
Mhirra Yung <lookatdmoon@gmail.com>
2017-12-04 13:04:57 -0600

    Updated with Heroku

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,27 @@
 // Setup code to connect Node to MySQL
 
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    port: 8080,
-    host: "localhost",
-    user: "root",
-    password: "ItsNotFriday!",
-    database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//     port: 8080,
+//     host: "localhost",
+//     user: "root",
+//     password: "ItsNotFriday!",
+//     database: "burgers_db"
+// });
+
+// Hooking Project with JawsDB
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "ItsNotFriday!",
+        database: "burgers_db"
+    });
+};
 
 // Make a connection
 connection.connect(function (err) {
